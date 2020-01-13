eca2ff0c91a01485b7a568e5acbe35b72d305a6f
Adelin Nedelcu <n.adelin0223@gmail.com>
2020-01-13 14:59:49 +0200

    Searchbar works now

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Recipe from './Recipe.js'
+import Recipe from './Components/Recipe.js'
 
 function App() {
 
   const APP_ID = '9aabad26';
   const APP_KEY = '701d534e360cf30c7f5db4376838156c';
 
-  const [query, setQuery] = useState('chicken');
+  const [query, setQuery] = useState('');
+  const [search, setSearch] = useState('');
 
   const [recipes, setRecipes] = useState([]);
 
@@ -17,23 +18,33 @@ function App() {
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   //get searched recipes
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
 
-    console.log(data.hits);
-
     setRecipes(data.hits);
   };
   
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const updateQuery = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
   return (
     <div className="App">
+      {/* </Navbar > */}
+
       <div className="d-flex justify-content-center m-2">
-        <form className="form-inline">
-          <input type="text" className="form-control" placeholder="Search for a recipe..."/>
+        <form onSubmit={updateQuery} className="form-inline">
+          <input type="text" className="form-control" placeholder="Search for a recipe..." value={search} onChange={updateSearch}/>
           <button type="submit" className="btn btn-primary ml-2">Search</button>
         </form>
       </div>
